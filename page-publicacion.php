074fee93584bccb10da2f547f4695a1dc5c7761c
vg oa <victor@pugsarrollos.com>
2018-03-22 11:01:11 -0600

    a ver si funciono

--- page-publicacion.php
+++ page-publicacion.php
@@ -5,18 +5,5 @@ Template Name: publicacion
 ?>
 <?php get_header(); ?>
 
-<div class="container-responsive mt-5">
-  <div class="row">
-
-    <div class="col-sm">
-      <div id="content" role="main">
-        <?php get_template_part('loops/page-content'); ?>
-      </div><!-- /#content -->
-    </div>
-
-    <?php get_sidebar(); ?>
-
-  </div><!-- /.row -->
-</div><!-- /.container-responsive -->
 
 <?php get_footer(); ?>
