b31745e7e30f84b3d881dc81f2bb845b2c370169
vg023 <victor@pugsarrollos.com>
2018-03-23 15:09:06 -0600

    creacion desecciones
     se creo la seccion de parallax y se aplico el estilo preliminar correspondiente.

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 	</div>	
 </div>
-<div class="container-felx">
+<div class="noticias container-felx">
   <div class="row">
     <div class="col-sm d-flex">
       <div id="content col-3" role="main">
@@ -32,5 +32,15 @@
     </div>
   </div><!-- /.row -->
 </div><!-- /.container-responsive -->
-
+<div class="parallax container-flex">	
+	<div class="para-conte container-flex">
+		<h1>¿Como puedo aportar?</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
 <?php get_footer(); ?>
--- theme/js/menu.js
+++ theme/js/menu.js
@@ -12,4 +12,6 @@ $(document).ready(function(){
 	$(".trigger").on("mouseleave",function(e){
 		$('.sub-menu').hide();
 	});
+	console.log($(window).innerHeight());
+	console.log($(window).outerHeight());
 });
