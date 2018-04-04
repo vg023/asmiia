e380caf73a782022e4c819afbea785a2c8c2b567
Gamo <devic23@outlook.com>
2018-04-04 11:38:39 -0500

    ajustes estilos.
     SE COMENZO A DAR FORMA A LAS SECCIONES CREADAS APLICANDO ALTERNATIVAS DE ESTILOS

--- index.php
+++ index.php
@@ -6,27 +6,27 @@
 	<img src="<?php bloginfo('template_url');?>/theme/img/microsco.jpeg">
 </div>
 <div class="quienes container-flex">
-	<h1>quienes somos</h1>
-	<p>lorem ipsum</p>
+	<h1>¿quienes somos?</h1>
+	<i class="fa fa-angle-double-down flecha-abajo"></i>
 	<div class="quienes-conte">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores error, quibusdam laudantium illum enim ratione distinctio, dolore fuga assumenda eos ullam quod eveniet, tenetur, possimus vero. Labore impedit quam quaerat veniam recusandae possimus totam, autem repellendus, delectus doloremque aliquam deleniti sapiente nobis accusamus harum perspiciatis in omnis. Maxime obcaecati, modi.
+		</p>
 	</div>	
 </div>
 <div class="noticias container-felx">
   <div class="row">
     <div class="col-sm d-flex">
-      <div id="content col-3" role="main">
+      <div id="content" role="main">
+      	<h1 class="titul-secc">Blog</h1>
         <?php get_template_part('loops/index-loop'); ?>
       </div><!-- /#content -->
-       <div id="content col-3" role="main">
+       <div id="content" role="main">
+        <h1 class="titul-secc">Articulos</h1>
         <?php get_template_part('loops/index-loop'); ?>
       </div><!-- /#content -->
-       <div id="content col-3" role="main">
+       <div id="content" role="main">
+        <h1 class="titul-secc">Noticias</h1>
         <?php get_template_part('loops/index-loop'); ?>
       </div><!-- /#content -->
     </div>
