7c50407193e39f8e408a6ba30e4fc12e48ff5f9a
CAMP87 <36607771+CAMP87@users.noreply.github.com>
2018-07-03 01:00:13 -0500

    Libro-estilos
    Se agregaron estilos para darle forma

--- single-publicaciones.php
+++ single-publicaciones.php
@@ -3,22 +3,40 @@
  * Single-publicaciones
  */
 ?>
+<div class="container-fluid">
+  <?php get_header(); ?>
+  <div class="title-pub">
+    <h1>
+      <?php the_title(); ?>
+    </h1>
+  </div>
+  <div class= "row conte-pub">
+    <div class="image-libro container col-sm-12 col-md-12 col-lg-6">
+      <a href="<?php the_field( 'url' ); ?>">
+        <?php if( get_field('imagen_de_portada') ): ?>
+          <img src="<?php the_field('imagen_de_portada'); ?>" />
+        <?php endif; ?>
+      </a>
+    </div>
+    <div class="info-libro container col-6 col-sm-12 col-md-12 col-lg-6">
+        <p>
+          <?php the_field('nombre');?>
+          <br><?php the_field('autor');?></br>
+          <ul>
+            <li><?php the_field('colaboradores');?></li>
+          </ul>
+          <br><?php the_field('editorial');?></br>
+          <br><?php the_field('isbn');?></br>
+          <br><?php the_field('edicion');?></br>
 
-<?php get_header(); ?>
-<div class="title-pub">
-  <h1>
-    <?php the_title(); ?>
-  </h1>
+        </p>
+    </div>
+    <div class="reseña-libro col-12 ">
+      <p>
+        <?php the_field('reseña_del_libro');?>
+      </p>
+      <a href="<?php the_field( 'url' ); ?>">Descargar Aquí</a>
+    </div>
+  </div>
 </div>
-<div class="conte-pub">
-  <h2>
-    <?php the_field('nombre');?>
-  </h2>
-  <h2>
-    <?php the_field('autor');?>
-  </h2>
-  <h2>
-    <?php the_field('isbn');?>
-  </h2>  
-</div>
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
