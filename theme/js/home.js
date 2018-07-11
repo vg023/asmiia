bfc0990732c9d91f22617d0c6c28dd387eb059bc
Gamo <v.coach23@outlook.com>
2018-07-10 23:41:04 -0500

    ajustes generales comienzo de animaciones
    se ajusto el archivo del scss de publicaciones y se ajustaron las animaciones en la flecha en laseccion quienes

--- theme/js/home.js
+++ theme/js/home.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 	$('.flecha-abajo').on("click",function(){
 		$('.quienes').toggleClass('comprimido');
+		$(this).fadeOut();
 	});
 	$('.flecha-arriba').on("click",function(){
 		$('.quienes').toggleClass('comprimido');
+		$('.flecha-abajo').fadeIn();
 	});
 	
 });
