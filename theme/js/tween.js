27a9e6f142a3e3ed842405005585d7a23c0cf8e0
Gamo <v.coach23@outlook.com>
2018-07-11 23:39:26 -0500

    edicion de animaciones
    se comenzo la edicion de las animaciones en el sitio

--- theme/js/tween.js
+++ theme/js/tween.js
@@ -5,8 +5,23 @@ $(document).ready(function(e){
 	tl.set(flechaAbajo, {opacity:0, y:100,repeat:-1,yoyo:true});
 	$(".quienes").on("mouseenter", function(){
 		tl.to(quienes,1.5, {opacity:"1"});		
-		tl.to(flechaAbajo,1.5,{opacity:1,y:0,repeat:-1,yoyo:true},'+1');
-			
+		tl.to(flechaAbajo,1.5,{opacity:1,y:0,repeat:-1,yoyo:true},'+1');		
+	});
+	$(".noticias").each(function(){
+		var conte = $(this).find("div#content");		
+		console.log(conte);
+		tl2= new TimelineMax({});
+		tl2.set(conte, {opacity:"0"});
+		tl2.set( $(".pub .container"), {x:500,y:500,opacity:"0"});	
+		tl2.set( $(".art .container-fluid .col"), {x:500,y:500,opacity:"0"});	
+	
+		$(window).on("scroll",function(e){
+			if($(this).scrollTop()>=$(".art").position().top)
+			{
+				tl2.to( $(".art .container-fluid .col"), 2, {x:0,y:0,opacity:"1"});
+			}
+		});
+
 	});
 
 });
