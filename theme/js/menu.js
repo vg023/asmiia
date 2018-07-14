b01b419941c20d822594b814000ae38a6bcdfeed
Gamo <v.coach23@outlook.com>
2018-07-14 01:57:54 -0500

    ajustes responsive

--- theme/js/menu.js
+++ theme/js/menu.js
@@ -1,19 +1,20 @@
     $(document).ready(function(){
-    while($(window).width()>=768){
-    	$(window).on("scroll",function(e){
+   	if ( $(window).width()>=760) {
+   		$(window).on("scroll",function(e){
 			if($(window).scrollTop()>200){
 				$('#nav-main').fadeOut('slow');
 			}else{
 				$('#nav-main').fadeIn('fast');
 			}
-		});
-		$(".menu-item-has-children").on("mouseenter",function(e){
-			$(this).children('.sub-menu').show();
-		});
-		$(".menu-item-has-children").on("mouseleave",function(e){
-			$('.sub-menu').hide();
 		});	
-    }
+   	} 	
+	
+	$(".menu-item-has-children").on("mouseenter",function(e){
+		$(this).children('.sub-menu').show();
+	});
+	$(".menu-item-has-children").on("mouseleave",function(e){
+		$('.sub-menu').hide();
+	});
 	console.log($(window).innerHeight());
 	console.log($(window).outerHeight());
 });
