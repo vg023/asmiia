8512e1b599b1193332451cf4465f87c86a2e2e87
Gamo <v.coach23@outlook.com>
2018-07-14 01:05:08 -0500

    integracion imagen responsive
    se incluye el acf para la version responsive del sitio en el slider.

--- theme/js/menu.js
+++ theme/js/menu.js
@@ -1,17 +1,19 @@
     $(document).ready(function(){
-	$(window).on("scroll",function(e){
-		if($(window).scrollTop()>200){
-			$('#nav-main').fadeOut('slow');
-		}else{
-			$('#nav-main').fadeIn('fast');
-		}
-	});
-	$(".menu-item-has-children").on("mouseenter",function(e){
-		$(this).children('.sub-menu').show();
-	});
-	$(".menu-item-has-children").on("mouseleave",function(e){
-		$('.sub-menu').hide();
-	});
+    while($(window).width()>=768){
+    	$(window).on("scroll",function(e){
+			if($(window).scrollTop()>200){
+				$('#nav-main').fadeOut('slow');
+			}else{
+				$('#nav-main').fadeIn('fast');
+			}
+		});
+		$(".menu-item-has-children").on("mouseenter",function(e){
+			$(this).children('.sub-menu').show();
+		});
+		$(".menu-item-has-children").on("mouseleave",function(e){
+			$('.sub-menu').hide();
+		});	
+    }
 	console.log($(window).innerHeight());
 	console.log($(window).outerHeight());
 });
